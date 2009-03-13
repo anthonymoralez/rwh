fc8718ef20e838fc1a883e46ce96118355408dfb
Anthony Moralez <anthony.moralez@gmail.com>
2009-03-13 15:45:45 -0700

    Chapter 6: Show from http://book.realworldhaskell.org/read/using-typeclasses.html

--- ch06/eqclasses.hs
+++ ch06/eqclasses.hs
@@ -25,3 +25,8 @@ instance BasicEq3 Color where
     isEqual3 Green Green = True
     isEqual3 Blue  Blue  = True
     isEqual3 _     _     = False
+
+instance Show Color where
+    show Red   = "Red"
+    show Green = "Green"
+    show Blue  = "Blue"
