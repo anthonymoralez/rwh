2e8937c18b3d038713165e4426c172e1958a455a
Anthony Moralez <anthony.moralez@gmail.com>
2009-03-16 16:13:25 -0700

    Chapter 6: JSON typeclasses without overlapping instances from http://book.realworldhaskell.org/read/using-typeclasses.html

--- ch06/JSONClass.hs
+++ ch06/JSONClass.hs
@@ -1,8 +1,10 @@
--- NOTE: you must add -i../ch05 to the ghc(i) command-line for this to work 
 {-# LANGUAGE TypeSynonymInstances #-}
-module JSONClass where 
+module JSONClass 
+    (
+      JAry(..)
+    ) where 
 
-import SimpleJSON
+import Control.Arrow (second)
 
 type JSONError = String
 
@@ -39,3 +41,60 @@ instance JSON Integer where
 instance JSON Double where
     toJValue   = JNumber
     fromJValue = doubleToJValue id
+
+newtype JAry a  = JAry {
+      fromJAry :: [a]
+    } deriving (Eq, Ord, Show)
+
+newtype JObj a = JObj {
+      fromJObj :: [(String, a)]
+    } deriving (Eq, Ord, Show)
+
+data JValue = JString String
+            | JNumber Double
+            | JBool Bool
+            | JNull
+            | JObject (JObj JValue) -- was [(String, JValue)]
+            | JArray (JAry JValue)  -- was [JValue]
+
+jaryFromJValue :: (JSON a) => JValue -> Either JSONError (JAry a)
+jaryFromJValue (JArray (JAry a)) =
+    whenRight JAry (mapEithers fromJValue a)
+jaryFromJValue _ = Left "not a JSON array"
+
+jaryToJValue :: (JSON a) => JAry a -> JValue
+jaryToJValue = JArray . JAry . map toJValue . fromJAry
+
+instance (JSON a) => JSON (JAry a) where
+    toJValue = jaryToJValue
+    fromJValue = jaryFromJValue
+
+listToJValues :: (JSON a) => [a] -> [JValue]
+listToJValues = map toJValue
+
+jvaluesToJAry :: [JValue] -> JAry JValue
+jvaluesToJAry = JAry
+
+jaryOfJValuesToJValue :: JAry JValue -> JValue
+jaryOfJValuesToJValue = JArray
+
+
+whenRight :: (b -> c) -> Either a b -> Either a c
+whenRight _ (Left err) = Left err
+whenRight f (Right a) = Right (f a)
+
+mapEithers :: (a -> Either b c) -> [a] -> Either b [c]
+mapEithers f (x:xs) = case mapEithers f xs of
+                        Left err -> Left err
+                        Right ys -> case f x of
+                                      Left err -> Left err
+                                      Right y -> Right (y:ys)
+mapEithers _ _ = Right []
+
+instance (JSON a) => JSON (JObj a) where
+    toJValue = JObject . JObj . map (second toJValue) . fromJObj
+
+    fromJValue (JObject (JObj o)) = whenRight JObj (mapEithers unwrap o)
+        where unwrap (k,v) = whenRight ((,) k) (fromJValue v)
+    fromJValue _ = Left "not a JSON object"
+
