616206c2060ed1bc116b9b9f56297276f6d67404
Anthony Moralez <anthony.moralez@gmail.com>
2009-03-17 15:58:12 -0700

    Chapter 7: The True Nature of Return from http://book.realworldhaskell.org/read/io.html

---
+++ ch07/return2.hs
@@ -0,0 +1,12 @@
+import Data.Char (toUpper)
+
+isYes :: String -> Bool
+isYes inpStr = (toUpper . head $ inpStr) == 'Y'
+
+isGreen :: IO Bool
+isGreen =
+    do putStrLn "Is green your favorite color?"
+       inpStr <- getLine
+       return (isYes inpStr)
+
+main = isGreen
---
+++ ch07/return3.hs
@@ -0,0 +1,7 @@
+returnTest :: IO ()
+returnTest = 
+    do one <- return 1
+       let two = 2
+       putStrLn $ show (one + two) 
+
+main = returnTest
