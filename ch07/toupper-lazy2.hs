ab16f1a7c23465b1dfb550c3d64d62903c4da14b
Anthony Moralez <anthony.moralez@gmail.com>
2009-03-17 15:35:28 -0700

    Chapter 7: Lazy I/O from http://book.realworldhaskell.org/read/io.html

---
+++ ch07/toupper-lazy2.hs
@@ -0,0 +1,10 @@
+import System.IO
+import Data.Char (toUpper)
+
+main = do 
+       inh <- openFile "input.txt" ReadMode
+       outh <- openFile "output.txt" WriteMode
+       inpStr <- hGetContents inh
+       hPutStr outh (map toUpper inpStr)
+       hClose inh
+       hClose outh
